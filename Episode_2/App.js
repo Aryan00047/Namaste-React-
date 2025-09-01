61fff4f50e47e41e6ac88053888a0ebc31369303
Aryan Gupta <aryan2k1.gupta@gmail.com>
2025-09-01 21:55:20 +0530

    Updated git

---
+++ Episode_2/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child 1", key: "1" }, [
+    React.createElement("h1", { key: "1" }, "Heading 1"),
+    React.createElement("h2", { key: "2" }, "Heading 2"),
+  ]),
+  React.createElement("div", { id: "Child 2", key: "2" }, [
+    React.createElement("h1", { key: "1" }, "Heading 1"),
+    React.createElement("h2", { key: "2" }, "Heading 2"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
